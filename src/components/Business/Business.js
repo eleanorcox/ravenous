983c1e7428b0267d7ed03730990c2405e8058f4b
eleanor <ec15613@my.bristol.ac.uk>
2020-06-07 14:15:32 +0100

    Completed Ravenous project

---
+++ src/components/Business/Business.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Business.css';
+
+class Business extends React.Component {
+    constructor(props){
+        super(props);
+        this.getAddressLink = this.getAddressLink.bind(this);
+    }
+
+    getAddressLink(){
+        let addr = `${this.props.business.address}+${this.props.business.city}+${this.props.business.zipCode}`;
+        addr = addr.replace(/ /g, '+');
+        const link = `https://www.google.com/maps/place/${addr}/`;
+        return link;
+    }
+
+    render(){
+       return   (
+        <div className="Business">
+            <div className="image-container">
+                <a href={this.props.business.website} target = "_blank" rel="noopener noreferrer">
+                    <img src={this.props.business.imageSrc} alt=''/>
+                </a>
+            </div>
+            <h2>{this.props.business.name}</h2>
+            <div className="Business-information">
+                <div className="Business-address">
+                    <a href={this.getAddressLink()} target="_blank" rel="noopener noreferrer">{this.props.business.address}</a>
+                    <p>{this.props.business.city}</p>
+                    <p>{this.props.business.state} {this.props.business.zipCode}</p>
+                </div>
+                <div className="Business-reviews">
+                <h3>{this.props.business.category}</h3>
+                <h3 className="rating">{this.props.business.rating} stars</h3>
+                <p>{this.props.business.reviewCount} reviews</p>
+                </div>
+            </div>
+        </div>
+       );
+    }
+}
+
+export default Business;
